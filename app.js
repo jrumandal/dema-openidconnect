923dab645789a0d1f6608fb7e4529f2b9ad9d26e
John Ralph Umandal <jrumandal@gmail.com>
2018-10-10 17:01:07 +0200

    Added error route

--- app.js
+++ app.js
@@ -106,6 +106,9 @@ app.use((req, res, next) => {
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/error', (req, res) => {
+    next();
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
