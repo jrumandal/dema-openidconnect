bae16b20e2d30685647fd6a6f5d1bea1622a607e
John Ralph Umandal <jrumandal@gmail.com>
2018-10-10 18:31:06 +0200

    try plain

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.use(express.static(path.join(__dirname, 'public')));
         params: {
             redirect_uri: 'https://dema-auth-test.herokuapp.com/callback'
         },
-        usePKCE: 'S256'
+        usePKCE: 'plain'
     }, (tokenset, tokenSecret, profile, done) => {
         console.log('tokenset', tokenset);
         console.log('access_token', tokenset.access_token);
