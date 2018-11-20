a594b081174a021bbad66529d8c1fd6d02e5ffa7
John Ralph Umandal <umandal_john_ralpf@network.lilly.com>
2018-11-20 15:05:21 +0100

    Testing auth without popup

--- public/main.js
+++ public/main.js
@@ -1,7 +1,12 @@
 
+$('.btnTriggerActionIframe').on('click', () => {
+    $('iframe').attr('src', 'API/DEL/000000');
+});
+
 $('.btnTriggerActionGet').on('click', () => {
     axios.get('API/DEL/000000');
 });
 $('.btnTriggerActionPost').on('click', () => {
     axios.post('API/DEL/111111');
 });
+
