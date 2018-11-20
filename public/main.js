de7fa20d7ce17683678489a2b5591404f5ac3407
John Ralph Umandal <umandal_john_ralpf@network.lilly.com>
2018-11-20 11:55:38 +0100

    Different code for get

--- public/main.js
+++ public/main.js
@@ -1,6 +1,6 @@
 $(window).on('ready', function () {
     $('.btnTriggerActionGet').on('click', () => {
-        axios.get('/DEL/111111');
+        axios.get('/DEL/000000');
     });
     $('.btnTriggerActionPost').on('click', () => {
         axios.post('/DEL/111111');
