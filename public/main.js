1415ba7b4c94481820ec6b67be6d43b694ce8dde
John Ralph Umandal <umandal_john_ralpf@network.lilly.com>
2018-11-20 16:16:39 +0100

    Test

--- public/main.js
+++ public/main.js
@@ -10,3 +10,13 @@ $('.btnTriggerActionPost').on('click', () => {
     axios.post('API/DEL/111111');
 });
 
+this._interval = setInterval(() => {
+    let authIframe = $('iframe')[0];
+    console.log(authIframe.src);
+    console.log(authIframe.src);
+    console.log(authIframe.contentDocument.body.innerHTML);
+    try {
+        let result = authIframe.contentDocument.body.innerHTML;
+        let resultObj = JSON.parse(result);
+    } catch (e) { console.error('not parseble'); }
+}, 500);
