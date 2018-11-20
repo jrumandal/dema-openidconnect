919b420e748aa8cd39996603e8ae22b366e6cacb
John Ralph Umandal <umandal_john_ralpf@network.lilly.com>
2018-11-20 11:27:44 +0100

    Taking track of action before auth. Also included http2 module for a test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', user: req.user, sessionUser: JSON.stringify(req.user), sessionInfo: JSON.stringify(req.session) });
+  res.render('index', { title: 'Express', user: req.user, sessionUser: JSON.stringify(req.user), sessionInfo: JSON.stringify(req.session), action: req.session.action });
+  req.session.action = undefined;
+  req.session.code = undefined;
+});
+
+router.post('/API/:action/:code', (req, res, next) => {
+    req.session.action = req.params.action;
+    req.session.code = req.params.code;
+    res.redirect('/auth');
 });
 
 module.exports = router;
