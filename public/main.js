ba79edc57f931320ec9620633febabbeb168178e
John Ralph Umandal <jrumandal@gmail.com>
2020-06-25 17:32:00 +0100

    Patched iframe access

--- public/main.js
+++ public/main.js
@@ -10,13 +10,14 @@ $('.btnTriggerActionPost').on('click', () => {
     axios.post('API/DEL/111111');
 });
 
+console.info("Define `debugFunction` in `this` or `window` to be able to make any middle action/debugging/logging.")
 this._interval = setInterval(() => {
     let authIframe = $('iframe')[0];
-    console.log(authIframe.src);
-    console.log(authIframe.src);
-    console.log(authIframe.contentDocument.body.innerHTML);
+
+    if (globalThis.debugFunction) globalThis.debugFunction();
+
     try {
-        let result = authIframe.contentDocument.body.innerHTML;
+        let result = authIframe.contentWindow.document.body.innerHTML;
         console.log(result);
         let resultObj = JSON.parse(result);
         console.log(resultObj);
